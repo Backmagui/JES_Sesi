fa31f1d1dc8b300f91ac93ccf3350017921da062
Maria Clara Garatini <maria.garatini.senai404@gmail.com>
2024-11-22 14:15:21 -0300

    maria 22/11

---
+++ JES_Sesi-main/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>JES-2024 | Interclasse do SESI de Cerquilho</title>
+</head>
+
+<body>
+    <header>
+        <div class="menu"><img src="./images/menu.png" alt="" width="50px"></div>
+        <div class="title">
+            <h1>JES-2024</h1>
+        </div> 
+        <a class="login" href='./Login/Pagina_login/entrar.php'>
+            <img src="./images/login.png" alt="" width="50px">
+        </a>        
+    </header>
+    <main>
+        <section class="main-score">
+            <div class="score">
+                <div class="top-score">
+                    <h1 class="modalidade-esporte">Esporte-Fem/Masc</h1>
+                    <h2 class="ao-vivo">Ao vivo</h2>
+                </div>
+                <div class="bottom-score">
+                    <div class="squad-score">
+                        <img src="./images/emblema.png" alt="" width="150px;">
+                        <p id="name">Nome</p>
+                    </div>
+                    <h1 class="placar" id="time1">1</h1>
+                    <h1 class="x">X</h1>
+                    <h1 class="placar" id="time1">1</h1>
+                    <div class="squad-score">
+                        <img src="./images/emblema.png" alt="" width="150px">
+                        <p id="name">Nome</p>
+                    </div>
+                    
+                </div>
+
+            </div>
+            <div class="live-player">
+                <div class="personal-stats">
+                    <img src="./images/user.png" alt="" width="80px">
+                    <div class="goal">
+                        <h1>GOLLLL</h1>
+                        <p>nome</p>
+                    </div>
+                </div>
+                <div class="personal-stats">
+                    <img src="./images/user.png" alt="" width="80px">
+                    <div class="goal">
+                        <h1>GOLLLL</h1>
+                        <p>nome</p>
+                    </div>
+                </div>
+                <div class="personal-stats">
+                    <img src="./images/user.png" alt="" width="80px">
+                    <div class="goal">
+                        <h1>GOLLLL</h1>
+                        <p>nome</p>
+                    </div>
+                </div>  
+            </div>
+        </section>
+        <section class="timeline">
+            <div class="timeline-card">
+                <div class="timeline-date">
+                    <H1>HOJE</H1>
+                    <p>14:00</p>
+                </div>
+                <div class="timeline-score">
+                    <div class="timeline-squad">
+                        <img src="./images/emblema.png" alt="" width="100px">
+                    </div>
+                    <h1>X</h1>
+                    <div class="timeline-squad">
+                        <img src="./images/emblema.png" alt="" width="100px">
+                    </div>
+                </div>
+                <div class="timeline-modalidade">
+                    <p>Esporte-Fem/Masc</p>
+                </div>
+            </div>
+            <div class="timeline-card">
+                <div class="timeline-date">
+                    <H1>HOJE</H1>
+                    <p>14:00</p>
+                </div>
+                <div class="timeline-score">
+                    <div class="timeline-squad">
+                        <img src="./images/emblema.png" alt="" width="100px">
+                    </div>
+                    <h1>X</h1>
+                    <div class="timeline-squad">
+                        <img src="./images/emblema.png" alt="" width="100px">
+                    </div>
+                </div>
+                <div class="timeline-modalidade">
+                    <p>Esporte-Fem/Masc</p>
+                </div>
+            </div>
+            <div class="timeline-card">
+                <div class="timeline-date">
+                    <H1>HOJE</H1>
+                    <p>14:00</p>
+                </div>
+                <div class="timeline-score">
+                    <div class="timeline-squad">
+                        <img src="./images/emblema.png" alt="" width="100px">
+                    </div>
+                    <h1>X</h1>
+                    <div class="timeline-squad">
+                        <img src="./images/emblema.png" alt="" width="100px">
+                    </div>
+                </div>
+                <div class="timeline-modalidade">
+                    <p>Esporte-Fem/Masc</p>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+
+</html>
