438dfc2605ee147d609e3cd2451ebd15381777b9
Maria Clara Garatini <maria.garatini.senai404@gmail.com>
2024-11-22 12:20:31 -0300

    22/11

---
+++ JES_Sesi-main/Login/Pagina_contador/contador.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="pontuacao.css">
+    <title>Pontos</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="text-header"><h1>Manipulação de Pontos</h1></div>
+            <img class="logo-senai" src=logo.jpg alt="">
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <button class="botao01" type="submit"><p>Time-01</p></button>
+            <button class="botao01" type="submit"><p>Time-02</p></button>
+        </div>
+        <br>
+        <br>
+        <div class="botoes">
+            <a class="login" href='../Pagina_inicializador/inicializador.php'>
+                <button class="botao02" type="submit"><p>Voltar</p></button>
+            </a>    
+            <a class="login" href='../Pagina_inicializador/inicializador.php'>
+                <button class="botao02" type="submit"><p>Concluir</p></button>
+            </a>  
+        </div>
+    </main>
+</body>
+</html>
