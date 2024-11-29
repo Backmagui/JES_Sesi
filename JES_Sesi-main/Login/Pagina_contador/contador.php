b0cf9198c37178d76bbcfdc44f7a9c5b39c3fe16
Augusto Ferrari <augusto.ferrari.senai404@gmail.com>
2024-11-29 15:31:09 -0300

    maria - 29/11

--- JES_Sesi-main/Login/Pagina_contador/contador.php
+++ JES_Sesi-main/Login/Pagina_contador/contador.php
@@ -1,33 +1,78 @@
+<?php
+require_once("pontos.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="pontuacao.css">
     <title>Pontos</title>
 </head>
+
 <body>
     <header>
-        <nav class="navbar">
-            <div class="text-header"><h1>Manipulação de Pontos</h1></div>
-            <img class="logo-senai" src=logo.jpg alt="">
-        </nav>
+        <button class="menu" onclick="Menu()"><img src="./img_cont/menu.png" alt="" width="40px"></button>
+        <ul class="vertical-menu" id="menuvert">
+            <li><a href="">Cadastrar Time</a></li>
+            <li><a href="">Cadastrar Jogador</a></li>
+            <li><a href="">Cadastrar Jogo</a></li>
+            <li><a href="../../index.php" id="logout"><img src="./img_cont/login.png" alt="" width="20px">Sair</a></li>
+        </ul>
+        <div class="title">
+            <h1>JES-2024</h1>
+        </div>
+        <div class="login"><img src="./img_cont/timbre_sesi_senai.png" alt="" width="300px"></div>
     </header>
     <main>
-        <div class="container">
-            <button class="botao01" type="submit"><p>Time-01</p></button>
-            <button class="botao01" type="submit"><p>Time-02</p></button>
+        <div class="contador">
+            <div class="time">
+                <div class="placar"><h1><?php echo $_SESSION['pontos1']; ?></h1></div>
+                    <div class="add-ponto">
+                        <a href="?action=adicionar1"><h1 id="ponto">&#43</h1></a>
+                        <a href="?action=diminuir1"><h1 id="ponto">&#8722</h1></a>
+                    </div>
+                <div class="nome">Time</div>
+            </div>
+            <h1 id="x">X</h1>
+            <div class="time">
+                <div class="placar"><h1><?php echo $_SESSION['pontos2']; ?></h1></div>
+                    <div class="add-ponto">
+                        <a href="?action=adicionar2"><h1 id="ponto">&#43</h1></a>
+                        <a href="?action=diminuir2"><h1 id="ponto">&#8722</h1></a>
+                    </div>                                           
+                <div class="nome">Time</div>
+            </div>
         </div>
-        <br>
-        <br>
         <div class="botoes">
-            <a class="login" href='../Pagina_inicializador/inicializador.php'>
-                <button class="botao02" type="submit"><p>Voltar</p></button>
-            </a>    
-            <a class="login" href='../Pagina_inicializador/inicializador.php'>
-                <button class="botao02" type="submit"><p>Concluir</p></button>
-            </a>  
+            <a href="../Pagina_inicializador/inicializador.php">
+            <button>Voltar</button></a>
+            <a href="../Pagina_inicializador/inicializador.php">
+            <button>Concluir</button></a>
         </div>
     </main>
 </body>
+<script>
+
+    function Menu() {
+    
+        var menu = document.getElementById("menuvert");
+
+        if (menu.style.display === "none") {
+            menu.style.display="block";
+        }
+
+        else{
+            menu.style.display="none";
+        }
+    }
+
+</script>
+<script>
+    // Atualiza os pontos dinamicamente no HTML
+    document.getElementById('pontos').textContent = <?= $pontosAtuais ?>;
+</script>
+
 </html>
