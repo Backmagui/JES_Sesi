9a29d08644ce5b82be71a36e6cda0ca6e4979305
Maria Clara Garatini <maria.garatini.senai404@gmail.com>
2025-03-20 09:18:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,14 +10,14 @@ require_once("./Paginas/Pagina_contador/pontos.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>JES-2024 | Interclasse do SESI de Cerquilho</title>
+    <title>JES-2025 | Interclasse do SESI de Cerquilho</title>
 </head>
 
 <body>
     <header>
         <img src="./images/timbre_sesi_senai.png" alt="" width="200px">
         <div class="title">
-            <h1>JES-2024</h1>
+            <h1>JES-2025</h1>
         </div>
         <div class="login"><a href="./Paginas/Pagina_login/entrar.php"><img src="./images/login.png" alt="" width="50px"></a></div>
     </header>
@@ -33,7 +33,7 @@ require_once("./Paginas/Pagina_contador/pontos.php");
                         <img src="./images/emblema.png" alt="" width="90px;">
                         <p id="name">Time 1</p>
                     </div>
-                    
+
                     <h1 class="placar" id="time1"><?php echo $_SESSION['pontos1']; ?></h1>
                     <h1 class="x">X</h1>
                     <h1 class="placar" id="time2"><?php echo $_SESSION['pontos2']; ?></h1>
